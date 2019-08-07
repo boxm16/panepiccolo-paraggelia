00052fe88b3236785322704f54485749ef3ec369
boxm16 <boxm16@yahoo.com>
2019-08-07 10:57:00 +0300

    Initial commit

--- src/java/Controllers/LabelController.java
+++ src/java/Controllers/LabelController.java
@@ -6,7 +6,7 @@
 package Controllers;
 
 import DBTools.LabelDao;
-import Models.Label;
+
 import Models.LabelsListItem;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
